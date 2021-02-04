e5f0d0a7de3574a0660f12d91f5b76e59bb3d65e
Marcos <etchebestmarcos@gmail.com>
2021-02-04 18:32:40 -0300

    Borre lo que hice pero istale algunos componentes mas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import SelectBus from './components/SelectBus';
 import Autobus from './img/autobus.svg';
 import LivePost from './components/LivePost';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
+/* firebase.initializeApp(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4vajhOmF5VIz7rbpQZAvGVCxX8butwR8",
+    authDomain: "getat-bus.firebaseapp.com",
+    projectId: "getat-bus",
+    storageBucket: "getat-bus.appspot.com",
+    messagingSenderId: "495811298719",
+    appId: "1:495811298719:web:113df861c741d559aaa9aa"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+} */
 
 function App() {
   return (
--- src/components/LivePost.js
+++ src/components/LivePost.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './LivePost.css';
 import SendIcon from '@material-ui/icons/Send';
-import {useState} from 'react';
 
 function LivePost() {
 
     return (
         <div className="liveChat">
+            
             <input type="text"/>
             <button className="liveChat__button"><SendIcon /></button>
         </div>
